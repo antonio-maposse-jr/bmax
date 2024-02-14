aad37981d9051a71ce1d3e4d7e7f3167d0c2f5c6
António José Maposse Júnior <76467785+antonio-maposse-jr@users.noreply.github.com>
2024-02-14 09:24:13 +0200

    Updated other request validations Category and subcategory

--- app/Http/Requests/SubcategoryRequest.php
+++ app/Http/Requests/SubcategoryRequest.php
@@ -25,7 +25,8 @@ class SubcategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'name' => 'required|min:5|max:255',
+            'category_id' => 'required'
         ];
     }
 
